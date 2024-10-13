e956fee7fccd99bb9d0cb201c2dcc20c2dd35fba
globski <gloriaogunsemore.com>
2024-10-13 23:32:05 +0000

    Created the floor function that takes a float parameter and returns its floor value as an integer.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module for mathematical operations.
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a floating point number.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The floored integer value of n.
+    """
+    return math.floor(n)
