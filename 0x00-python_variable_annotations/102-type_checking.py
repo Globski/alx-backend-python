45b7fca603cad2dd50e45835527d5e42ced4f282
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-13 17:25:54 -0700

    Added type annotations to the zoom_array function to specify parameter and return types.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -19,8 +19,6 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     zoomed_in: List = [
         item for item in lst
-        for i
-
- in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
