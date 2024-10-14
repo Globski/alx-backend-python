cdd02db24e2efcd9c1dba30c043f0343d19e42a5
globski <gloriaogunsemore.com>
2024-10-14 00:18:21 +0000

    Added type annotations to the zoom_array function to specify parameter and return types.

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module for safely retrieving values from dictionaries.
+"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Safely retrieve a value from a dictionary.
+
+    Args:
+        dct (Mapping): The dictionary to retrieve from.
+        key (Any): The key to look for.
+        default (Union[T, None]): The value to return if the key is not found.
+
+    Returns:
+        Union[Any, T]: The value from the dictionary or the default value.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
