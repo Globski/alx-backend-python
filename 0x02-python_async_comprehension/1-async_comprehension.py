183b892e219e9985dfd33396fa2ed6a55ee19c2a
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-15 08:07:14 -0700

    Add async_comprehension: Implement a coroutine to collect 10 random numbers using async comprehension from async_generator.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,6 @@ from .async_generator import async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collects 10 random numbers using async comprehension over async_generator."""
+    """Collects 10 random numbers using async
+    comprehension over async_generator."""
     return [number async for number in async_generator()]
