5533eabe92c0efdbaeb5af8a8e45d826785e858b
Gloriana <141042662+Globski@users.noreply.github.com>
2024-10-15 08:55:41 -0700

    Add async_generator: Create an asynchronous generator that yields 10 random numbers between 0 and 10 with a 1-second delay.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,11 +5,11 @@ Module for asynchronous number generation.
 
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Asynchronous generator that yields 10
     random numbers between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
