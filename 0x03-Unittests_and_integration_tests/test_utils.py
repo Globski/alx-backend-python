50301bf2b8150d81795d69eb87c2f3f6688fcee8
Gloriana <141042662+Globski@users.noreply.github.com>
2024-11-09 21:48:41 -0800

    Add test for mocking HTTP calls in get_json.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from collections.abc import Mapping
 from typing import Any, Sequence
-from utils import access_nested_map
+from utils import (access_nested_map, get_json, memoize)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -36,6 +37,33 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(context.exception), repr(path[-1]))
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test cases for the get_json function from utils.py.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test that get_json correctly calls requests.get and returns the expected payload.
+        """
+
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
